86a80a537248c4e3a295f510228d212be02f493b
Midwo <michal.dwojak92@gmail.com>
2018-11-05 23:48:03 +0100

    Add: class (category and product) in models

---
+++ DemoShop/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoShop.Models
+{
+    public class Product
+    {
+
+        public int ProductID { get; set; }
+        
+        //field with virtual class - key 
+        public int ProductID { get; set; }
+
+        public string ProductTitle { get; set; }
+        public DateTime AddDate { get; set; }
+        public string  
+    }
+}
