60d950017fb0d24b42cbe70b89270be99005d8f0
Midwo <michal.dwojak92@gmail.com>
2018-11-09 00:00:27 +0100

    Add: class order

---
+++ DemoShop/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoShop.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateShipped { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public string CityCode { get; set; }
+        public string Street { get; set; }
+        public string ApartmentNumber {get; set;}
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public decimal SummaryPrice { get; set; }
+
+    }
+
+    public enum Country
+    {
+        [Display(Name ="Polska")]
+        Poland,
+        [Display(Name ="Niemcy")]
+        Deutchland
+
+
+    }
+}
