f10814bea65b43c035d9b97f32d46c63db0ac264
Midwo <michal.dwojak92@gmail.com>
2018-11-30 23:45:19 +0100

    add: changepassword and setpassword (View and method in controller)

--- DemoShop/Controllers/ManageController.cs
+++ DemoShop/Controllers/ManageController.cs
@@ -1,7 +1,9 @@
 ﻿using DemoShop.App_Start;
 using DemoShop.Models;
+using DemoShop.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +16,7 @@ namespace DemoShop.Controllers
     public class ManageController : Controller
     {
        
-        public async Task<ActionResult> ProfileAddress()
+        public async Task<ActionResult> ProfileAddress(ManageMessageId? message)
         {
             var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
             if (user == null)
@@ -22,13 +24,50 @@ namespace DemoShop.Controllers
                 return View("Error");
             }
 
+            ViewBag.StatusMessage =
+             message == ManageMessageId.SavedInformation ? "Zapisano zmiany"
+             : message == ManageMessageId.Error ? "Błąd - nie wykonano operacji"
+             : "";
+
             return View(user.UserInformation);
         }
 
-        public ActionResult Index()
+
+        public ActionResult Index(ManageMessageId? message)
         {
-            return View();
+            ViewBag.StatusMessage =
+                message == ManageMessageId.ChangePasswordSuccess ? "Hasło zostało zmienione"
+                : message == ManageMessageId.SetPasswordSuccess ? "Hasło zostało ustawione"
+                : message == ManageMessageId.Error ? "Błąd - nie wykonano operacji"
+                : "";
+            var model = new OtherLogInViewModel
+            {
+                HasPassword = HasPassword()
+            };
+         
+            return View(model);
         }
+        public async Task<ActionResult> OtherLogIn()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
+            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
+
+            var model = new OtherLogInViewModel
+            {
+                OtherLogins = otherLogins,
+                CurrentLogins = userLogins,
+                ShowRemoveButton = user.PasswordHash != null || otherLogins.Count > 1
+            };
+
+            return View(model);
+        }
+
+
         private ApplicationUserManager _userManager;
         public ApplicationUserManager UserManager
         {
@@ -46,24 +85,31 @@ namespace DemoShop.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> ChangeProfile(UserInformation userData)
         {
+            ManageMessageId message;
             if (ModelState.IsValid)
             {
 
                 var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
                 user.UserInformation = userData;
                 var result = await UserManager.UpdateAsync(user);
-
+                message = ManageMessageId.SavedInformation;
                 AddErrors(result);
+
+            }
+            else
+            {
+                message = ManageMessageId.Error;
 
             }
 
             if (!ModelState.IsValid)
             {
-                TempData["ViewData"] = ViewData;
-                return RedirectToAction("Index");
+                message = ManageMessageId.Error;
+                //TempData["ViewData"] = ViewData;
+                return RedirectToAction("ProfileAddress", new { Message = message });
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("ProfileAddress", new { Message = message });
         }
 
         private void AddErrors(IdentityResult result)
@@ -73,6 +119,168 @@ namespace DemoShop.Controllers
                 ModelState.AddModelError("", error);
             }
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LinkLogin(string provider)
+        {
+            // Request a redirect to the external login provider to link a login for the current user
+            return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"), User.Identity.GetUserId());
+        }
+
+        // Used for XSRF protection when adding external logins
+        private const string XsrfKey = "XsrfId";
+
+        public async Task<ActionResult> LinkLoginCallback()
+        {
+            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
+            if (loginInfo == null)
+            {
+                //return RedirectToAction("Index", new { Message = ManageMessageId.Error });
+                return RedirectToAction("Index");
+            }
+            var result = await UserManager.AddLoginAsync(User.Identity.GetUserId(), loginInfo.Login);
+            //return result.Succeeded ? RedirectToAction("Index", new { Message = ManageMessageId.LinkSuccess }) : RedirectToAction("Index", new { Message = ManageMessageId.Error });
+            return result.Succeeded ? RedirectToAction("Index") : RedirectToAction("Index");
+
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+            LinkSuccess,
+            Error,
+            SavedInformation
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
+        {
+            //ManageMessageId? message;
+            var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    await SignInAsync(user, isPersistent: false);
+                }
+                //message = ManageMessageId.RemoveLoginSuccess;
+            }
+            else
+            {
+                //message = ManageMessageId.Error;
+            }
+            //return RedirectToAction("OtherLogIn", new { Message = message });
+            return RedirectToAction("OtherLogIn");
+        }
+        private async Task SignInAsync(ApplicationUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie, DefaultAuthenticationTypes.TwoFactorCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = isPersistent }, await user.GenerateUserIdentityAsync(UserManager));
+        }
+        // GET: /Manage/ChangePassword
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Manage/ChangePassword
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                }
+                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+            }
+            AddErrors(result);
+            return View(model);
+        }
+
+
+        public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+        private ApplicationSignInManager _signInManager;
+        public ManageController()
+        {
+        }
+
+        // GET: /Manage/SetPassword
+        public ActionResult SetPassword()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Manage/SetPassword
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SetPassword(SetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await UserManager.AddPasswordAsync(User.Identity.GetUserId(), model.NewPassword);
+                if (result.Succeeded)
+                {
+                    var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                    if (user != null)
+                    {
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    }
+                    return RedirectToAction("Index", new { Message = ManageMessageId.SetPasswordSuccess });
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        private bool HasPassword()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            if (user != null)
+            {
+                return user.PasswordHash != null;
+            }
+            return false;
+        }
+
 
     }
 }
---
+++ DemoShop/ViewModels/ManageViewModels.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoShop.ViewModels
+{
+   public class OtherLogInViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+        public bool ShowRemoveButton { get; set; }
+        public SetPasswordViewModel SetPasswordViewModel { get; set; }
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public bool HasPassword { get; set; }
+    }
+
+
+    public class SetPasswordViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Hasłu musi mieć minimum {0} znaków oraz maksymalnie {2} ", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nowe hasło:")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Powtórz nowe hasło:")]
+        [Compare("NewPassword", ErrorMessage = "Hasła nie są takie same")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Stare hasło:")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Hasłu musi mieć minimum {0} znaków oraz maksymalnie {2} ", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nowe hasło:")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Powtórz nowe hasło:")]
+        [Compare("NewPassword", ErrorMessage = "Hasła nie są takie same")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
